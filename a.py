2f389e5a3debeb11a3c222a9bbc546cd9edd34d9
JB Silva <jbsilva.dev@outlook.com>
2025-01-18 16:23:13 -0300

    ajustando configuraçãos do assistente para retornar informações conforme solicitado

--- a.py
+++ a.py
@@ -1,8 +1,23 @@
-import dotenv
-import os
+from langchain.prompts import ChatPromptTemplate
+from models import configureIA as config
 
-# dotenv.load_dotenv('.env')
+for arquivo in config.pdfs_pendentes:
+    pdf = config.Path(rf"{config.caminho}/{arquivo}")
+    loader = config.PyPDFLoader(pdf)
+    dados_pdf = loader.load()
+    for doc in dados_pdf:
+        config.conteudo_pdf += doc.page_content
+    template = ChatPromptTemplate.from_messages([
+        (
+            "system",
+            "Voce é um assistente chamado J-bote, e a partir de agora tem acesso ao {conteudo_pdf}"
+        ),
+        ('user', '{input}'),
+    ])
 
-# Supondo que você tenha uma variável chamada 'MY_VARIABLE' no arquivo .env
-my_variable = os.getenv('api_key')
-print(my_variable)
+    chain = template | config.chat
+
+    resposta = chain.invoke({'conteudo_pdf': config.conteudo_pdf, 'input': 'Informe dados do documento disponibilizado, quero informações como distribuida, codigo de barras, codigo da instalação/unidade consumidora/conta contrato, valor a pagar, vencimento, emissao, numero da nota fiscal e mes refencia, traga estas informações em formato de tabela'})
+
+    print(resposta.content)
+    break
--- models/configureIA.py
+++ models/configureIA.py
@@ -5,34 +5,33 @@ from langchain_community.document_loaders import PyPDFLoader
 from pathlib import Path
 
 
-pdfs_pendentes = os.listdir(Path(r'..\src\pending'))
+caminho = Path(r'..\src\pending')
+pdfs_pendentes = os.listdir(caminho)
 conteudo_pdf = ''
+dados_capturados_pdf = {}
 
 os.environ['GROQ_API_KEY'] = os.getenv('API_KEY')
 
 chat = ChatGroq(model='llama-3.3-70b-versatile')
 
 for arquivo in pdfs_pendentes:
-  pdf = Path(rf"{pdfs_pendentes}\{arquivo}")
-  loader = PyPDFLoader(pdf)
-  dados_pdf = loader.load()
+    pdf = Path(rf"{caminho}/{arquivo}")
+    loader = PyPDFLoader(pdf)
+    dados_pdf = loader.load()
+    for doc in dados_pdf:
+        conteudo_pdf += doc.page_content
 
-  for doc in dados_pdf:
-    conteudo_pdf += doc.page_content
+    template = ChatPromptTemplate.from_messages([
+        (
+            "system",
+            "Voce é um assistente chamado J-bote, e a partir de agora tem acesso ao {conteudo_pdf}"
+        ),
+        ('user', '{input}'),
+    ])
 
+    chain = template | chat
 
-template = ChatPromptTemplate.from_messages([
-    (
-        "system",
-        "Voce é um assistente chamado J-bote, e a partir de agora tem acesso ao {conteudo_pdf}"
-    ),
-    ('user', '{input}'),
-])
+    resposta = chain.invoke({'conteudo_pdf': conteudo_pdf, 'input': 'Informe dados do documento disponibilizado, quero informações como distribuida, codigo de barras, codigo da instalação/unidade consumidora/conta contrato, valor a pagar, vencimento, emissao, numero da nota fiscal e mes refencia, traga estas informações em formato de tabela'})
 
-chain = template | chat
-
-resposta = chain.invoke({'documento_site': conteudo_pdf, 'input': 'Informe dados do documento disponibilizado, quero informações como docigo de barras, codigo da instalação/unidade consumidora/conta contrato, valor a pagar, vencimento, emissao, numero da nota fiscal e mes refencia'})
-
-print(resposta.content)
-
+    print(resposta.content)
 
